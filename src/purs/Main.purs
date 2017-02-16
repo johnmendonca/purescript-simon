bd24749e67e774fed390f4368a8d6b2c8244a1e4
John Mendonca <joaosinho@gmail.com>
2017-02-15 18:50:15 -0700

    Add type and imports

--- src/purs/Main.purs
+++ src/purs/Main.purs
@@ -1,6 +1,9 @@
 module Main where
 
-import Control.Monad.Eff.Console
+import Prelude (Unit)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
 
+main :: ∀ t1. Eff ( console ∷ CONSOLE | t1 ) Unit
 main = log "Hello"
 
