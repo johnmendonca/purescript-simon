c437c1aa95ef92c9fb680c8591d96edf632813c9
John Mendonca <joaosinho@gmail.com>
2017-08-26 16:21:34 -0700

    Generate a list of random numbers

--- src/purs/Main.purs
+++ src/purs/Main.purs
@@ -1,9 +1,15 @@
 module Main where
 
-import Prelude (Unit)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude
 
-main :: ∀ t1. Eff ( console ∷ CONSOLE | t1 ) Unit
-main = log "Hello"
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Control.Monad.Eff.Random (RANDOM, randomInt)
+import Data.Unfoldable (replicateA)
+
+main :: ∀ t1. Eff ( console :: CONSOLE, random :: RANDOM | t1 ) Unit
+main = do
+  randoms :: Array Int <- replicateA 20 $ randomInt 1 40
+  logShow randoms
+  pure unit
 
