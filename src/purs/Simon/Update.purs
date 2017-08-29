9d2f5c9c43f6899365d14bec2ba0441fbc9918c0
John Mendonca <joaosinho@gmail.com>
2017-08-29 01:31:54 -0700

    Part 5: main gameplay logic

--- src/purs/Simon/Update.purs
+++ src/purs/Simon/Update.purs
@@ -8,30 +8,98 @@ module Simon.Update
 
 import Prelude
 
-import Simon.Colors (Color)
-
 import Control.Monad.Aff (delay)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Data.Maybe (Maybe(..))
+import Control.Monad.Eff.Random (RANDOM)
+import Data.Array (length, range, snoc, zipWith)
+import Data.Foldable (notElem)
+import Data.Int (toNumber)
+import Data.Maybe (Maybe(Just, Nothing))
 import Data.Time.Duration (Milliseconds(..))
-import Pux (EffModel, noEffects)
+import Pux (EffModel, noEffects, onlyEffects)
+import Simon.Colors (Color, randomColors)
 
-type AppEffects = (console :: CONSOLE)
+type AppEffects = (console :: CONSOLE, random :: RANDOM)
 
 type State =
   { currentColor :: Maybe Color
+  , sequence :: Array Color
+  , count :: Int
+  , userClicks :: Array Color
   }
 
 initialState :: State
-initialState = { currentColor: Nothing }
+initialState =
+  { currentColor: Nothing
+  , sequence: []
+  , count: 0
+  , userClicks: []
+  }
 
-data Event = UserClick Color | ResetColor
+data Event
+  = UserClick Color
+  | ResetColor
+  | StartGame
+  | NextSequence (Array Color)
+  | AnimateColor Color
+  | PlaySequence
 
 foldp :: Event -> State -> EffModel State Event AppEffects
 foldp (UserClick color) state =
+  let
+    newClicks = snoc state.userClicks color
+    winning   = notElem false (zipWith (==) newClicks state.sequence)
+    fullMove  = length newClicks == state.count
+    finished  = length newClicks == length state.sequence
+  in
+    if fullMove && winning && finished then
+      onlyEffects state
+        [ pure $ Just $ AnimateColor color
+        , pure $ Just StartGame
+        ]
+    else if fullMove && winning then
+      { state: state { count = state.count + 1, userClicks = [] }
+      , effects:
+        [ pure $ Just $ AnimateColor color
+        , pure $ Just PlaySequence
+        ]
+      }
+    else if winning then
+      { state: state { userClicks = newClicks }
+      , effects: [ pure $ Just $ AnimateColor color ]
+      }
+    else
+      onlyEffects state
+        [ pure $ Just $ AnimateColor color
+        , pure $ Just StartGame
+        ]
+
+foldp ResetColor state = noEffects state
+  { currentColor = Nothing }
+
+foldp StartGame state = onlyEffects state
+  [ do
+      colors <- liftEff $ randomColors 20
+      pure $ Just $ NextSequence colors
+  ]
+
+foldp (NextSequence colors) state =
+  { state: state { sequence = colors, count = 1, userClicks = [] }
+  , effects:
+    [ pure $ Just PlaySequence ]
+  }
+
+foldp (AnimateColor color) state =
   { state: state { currentColor = Just color }
   , effects:
     [ delay (Milliseconds 300.0) $> Just ResetColor ]
   }
 
-foldp ResetColor state = noEffects state { currentColor = Nothing }
+foldp PlaySequence state = onlyEffects state $
+  zipWith fn (range 1 state.count) state.sequence
+  where
+    fn index color = do
+      delay $ Milliseconds (toNumber index * 1000.0)
+      pure $ Just $ AnimateColor color
+
--- src/purs/Simon/View.purs
+++ src/purs/Simon/View.purs
@@ -2,16 +2,14 @@ module Simon.View
   ( view
   ) where
 
+import Data.Maybe (Maybe(..))
 import Prelude hiding (div)
-
+import Pux.DOM.Events (onClick)
+import Pux.DOM.HTML (HTML)
 import Simon.Colors (Color(..))
 import Simon.Styles (styledButton)
 import Simon.Update (State, Event(..))
-
-import Data.Maybe (Maybe(..))
-import Pux.DOM.Events (onClick)
-import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (div, span)
+import Text.Smolder.HTML (button, div, span)
 import Text.Smolder.Markup (text, (!), (#!))
 
 activeCheck :: Maybe Color -> Color -> Boolean
@@ -27,10 +25,12 @@ view { currentColor } =
     simonButton Blue
     simonButton Green
     simonButton Yellow
+    startButton
   where
     simonButton color =
       span
         !  styledButton color (activeCheck currentColor color)
         #! onClick (const $ UserClick color)
         $  text ""
+    startButton = button #! onClick (const StartGame) $ text "Start"
 
